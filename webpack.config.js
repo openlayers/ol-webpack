07cac0ee312dd9af3d8b5b5c5353b5db1fce3611
Tim Schaub <tim.schaub@gmail.com>
2017-01-03 14:41:31 -0700

    Minify with Uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: './main.js',
   output: {
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
