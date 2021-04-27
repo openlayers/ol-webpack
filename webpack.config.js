3844399a0333c29fd008194c7a76d631cb4089d9
Tim Schaub <tim.schaub@gmail.com>
2021-04-26 19:36:51 -0600

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   entry: './main.js',
   output: {
     path: __dirname,
